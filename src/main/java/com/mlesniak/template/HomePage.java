06334494ab046009ef2962fdcc1ba9f240f9a8ae
Michael Lesniak <mail@mlesniak.com>
2013-12-15 14:42:07 +0100

    Bootstrap theme added.

--- src/main/java/com/mlesniak/template/HomePage.java
+++ src/main/java/com/mlesniak/template/HomePage.java
@@ -1,12 +1,10 @@
 package com.mlesniak.template;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
     public HomePage(final PageParameters parameters) {
         super(parameters);
-        add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
     }
 }
