4c051abc68c5322c5a4232c4bada1f42c7140b68
Michael Lesniak <mail@mlesniak.com>
2013-12-15 15:04:21 +0100

    Wicket handling of form input.

--- src/main/java/com/mlesniak/template/HomePage.java
+++ src/main/java/com/mlesniak/template/HomePage.java
@@ -1,10 +1,26 @@
 package com.mlesniak.template;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
     public HomePage(final PageParameters parameters) {
         super(parameters);
+
+        final Message message = new Message();
+
+        Form<Message> messageForm = new Form<Message>("messageForm", new CompoundPropertyModel<>(message)) {
+            @Override
+            protected void onSubmit() {
+                System.out.println(message);
+            }
+        };
+        TextField<String> input = new TextField<>("message");
+
+        add(messageForm);
+        messageForm.add(input);
     }
 }
---
+++ src/main/java/com/mlesniak/template/Message.java
@@ -0,0 +1,24 @@
+package com.mlesniak.template;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Entity
+public class Message implements Serializable {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
