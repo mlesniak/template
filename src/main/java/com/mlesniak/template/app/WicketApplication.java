546441dd558a167747210d470ac74c0c58cc7e39
Michael Lesniak <mail@mlesniak.com>
2013-12-22 00:19:27 +0100

    Initial starting message.

--- src/main/java/com/mlesniak/template/app/WicketApplication.java
+++ src/main/java/com/mlesniak/template/app/WicketApplication.java
@@ -3,8 +3,12 @@ package com.mlesniak.template.app;
 import com.mlesniak.template.config.Config;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WicketApplication extends WebApplication {
+    private Logger log = LoggerFactory.getLogger(WicketApplication.class);
+
     @Override
     public Class<? extends WebPage> getHomePage() {
         return HomePage.class;
@@ -13,6 +17,7 @@ public class WicketApplication extends WebApplication {
     @Override
     public void init() {
         super.init();
+        log.info("Starting application.");
         Config.get().init();
 
         mountPage("/config", ConfigPage.class);
