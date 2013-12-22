88434a0093a233bfa5e49006ee5a9bda6b30831f
Michael Lesniak <mail@mlesniak.com>
2013-12-22 01:23:20 +0100

    Automatic reloading.

--- src/main/java/com/mlesniak/template/config/ConfigPanel.java
+++ src/main/java/com/mlesniak/template/config/ConfigPanel.java
@@ -43,6 +43,7 @@ public class ConfigPanel extends Panel {
                 }
 
                 if (reload) {
+                    getSession().invalidateNow();
                     setResponsePage(getPage());
                 }
             }
