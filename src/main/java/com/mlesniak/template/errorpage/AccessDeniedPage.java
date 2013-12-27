1a400cb7e8725fd02b77364d6cd95d1ee4c6a61e
Michael Lesniak <mail@mlesniak.com>
2013-12-27 09:18:50 +0100

    Fixed navbar bug.

--- src/main/java/com/mlesniak/template/errorpage/AccessDeniedPage.java
+++ src/main/java/com/mlesniak/template/errorpage/AccessDeniedPage.java
@@ -1,10 +1,8 @@
 package com.mlesniak.template.errorpage;
 
 import com.mlesniak.template.BasePage;
-import com.mlesniak.template.navigation.NavigationBarPanel;
 
 public class AccessDeniedPage extends BasePage {
     public AccessDeniedPage() {
-        add(new NavigationBarPanel("navigationBar"));
     }
 }
