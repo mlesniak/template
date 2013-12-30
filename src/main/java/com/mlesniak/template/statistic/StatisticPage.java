fb3e66b115a54a52e02170a856a47ac12fb09edf
Michael Lesniak <mail@mlesniak.com>
2013-12-30 07:52:43 +0100

    Check for admin.

--- src/main/java/com/mlesniak/template/statistic/StatisticPage.java
+++ src/main/java/com/mlesniak/template/statistic/StatisticPage.java
@@ -1,11 +1,12 @@
 package com.mlesniak.template.statistic;
 
 import com.mlesniak.template.BasePage;
+import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// @AuthorizeInstantiation("ADMIN")
+@AuthorizeInstantiation("ADMIN")
 public class StatisticPage extends BasePage {
     private Logger log = LoggerFactory.getLogger(StatisticPage.class);
 
