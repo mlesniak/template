38ad8d8c1bc26a14d24cc65b485e8727f6ff3fc9
Michael Lesniak <mail@mlesniak.com>
2014-01-07 05:44:46 +0100

    Working SMS sending w/o UTF-8.

--- src/main/java/com/mlesniak/template/sms/SMSService.java
+++ src/main/java/com/mlesniak/template/sms/SMSService.java
@@ -4,11 +4,9 @@ import com.mlesniak.template.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
 
 import static com.mlesniak.template.config.ConfigKeys.*;
@@ -30,29 +28,48 @@ public class SMSService {
 
     public void sendSMS(String to, String text) {
         Config config = Config.get();
-        String s = null;
-        try {
-            s = "https://rest.nexmo.com/sms/json?api_key=" + config.get(SMS_API) + "&api_secret=" +
-                    config.get(SMS_SECRET) + "&from=" + config.get(SMS_FROM) + "&to=" + to + "&text=" +
-                    URLEncoder.encode(text, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            log.error("Unsupported encoding.", e);
-        }
-
         URL url = null;
         try {
-            url = new URL(s);
+            url = generateURL(to, text, config);
+        } catch (UnsupportedEncodingException e) {
+            log.error("Unsupported encoding.", e);
+            return;
         } catch (MalformedURLException e) {
             log.error("Malformed url.", e);
+            return;
         }
 
         try {
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.connect();
+            String result = getURLContent(url);
+            System.out.println(result);
         } catch (IOException e) {
             log.error("Unable to connect.", e);
         }
 
         log.info("Sent SMS. to=" + to + " msg='" + text + "'");
+    }
+
+    private URL generateURL(String to, String text, Config config) throws UnsupportedEncodingException, MalformedURLException {
+        URL url;
+        String s = "https://rest.nexmo.com/sms/json?api_key=" + config.get(SMS_API) + "&api_secret=" +
+                config.get(SMS_SECRET) + "&from=" + config.get(SMS_FROM) + "&to=" + to + "&text=" +
+                URLEncoder.encode(text, "UTF-8");
+        url = new URL(s);
+        return url;
+    }
+
+    private String getURLContent(URL url) throws IOException {
+        InputStream inputStream = url.openConnection().getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        StringBuffer sb = new StringBuffer();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+            sb.append('\n');
+        }
+        reader.close();
+
+        return sb.toString();
     }
 }
