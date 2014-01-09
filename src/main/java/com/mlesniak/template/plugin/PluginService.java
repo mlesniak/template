0f49192777d44c3acf4e47965f7cca0c6712b7a7
Michael Lesniak <mail@mlesniak.com>
2014-01-09 06:49:55 +0100

    Use a cache to prevent continous loading.

--- src/main/java/com/mlesniak/template/plugin/PluginService.java
+++ src/main/java/com/mlesniak/template/plugin/PluginService.java
@@ -9,10 +9,15 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PluginService {
     private Logger log = LoggerFactory.getLogger(PluginService.class);
     private static PluginService INSTANCE;
+
+    // Map from interface to loaded class.
+    private Map<Class, Class> cache = new HashMap<>();
 
     public static PluginService get() {
         if (INSTANCE == null) {
@@ -22,24 +27,44 @@ public class PluginService {
         return INSTANCE;
     }
 
+    public void resetCache() {
+        cache.clear();
+    }
+
     @SuppressWarnings("unchecked")
     public <T> T getPlugin(Class<T> iface) {
-        String directory = Config.get().get(ConfigKeys.PLUGIN_DIRECTORY);
+        Class clazz = null;
         PluginRegistry plugin = PluginRegistry.get(iface);
-        File jarFile = new File(directory + "/" + plugin + ".jar");
-        log.info("Loading plugin: " + plugin + " ,path=" + directory + "/" + plugin + ".jar");
+
+        if (cache.containsKey(iface)) {
+            clazz = cache.get(iface);
+        } else {
+            clazz = loadClass(plugin);
+        }
 
         try {
-            URL[] urls = {new URL("jar:file:" + jarFile.getAbsoluteFile() + "!/")};
-            ClassLoader cl = URLClassLoader.newInstance(urls, getClass().getClassLoader());
-            Class clazz = cl.loadClass(plugin.getMainClass());
-            return (T)(clazz.newInstance());
-        } catch (MalformedURLException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            return (T) (clazz.newInstance());
+        } catch (InstantiationException | IllegalAccessException e) {
             log.error("Unable to load plugin. plugin=" + plugin, e);
         }
 
         return null;
     }
 
+    private Class loadClass(PluginRegistry plugin) {
+        Class clazz = null;
 
+        String directory = Config.get().get(ConfigKeys.PLUGIN_DIRECTORY);
+        File jarFile = new File(directory + "/" + plugin + ".jar");
+        log.info("Loading plugin: " + plugin + " ,path=" + directory + "/" + plugin + ".jar");
+        try {
+            URL[] urls = {new URL("jar:file:" + jarFile.getAbsoluteFile() + "!/")};
+            ClassLoader cl = URLClassLoader.newInstance(urls, getClass().getClassLoader());
+            clazz = cl.loadClass(plugin.getMainClass());
+
+        } catch (MalformedURLException | ClassNotFoundException e) {
+            log.error("Unable to load plugin. plugin=" + plugin, e);
+        }
+        return clazz;
+    }
 }
