f70e1746fa056371f88f7ad21f41b899bd03774c
Michael Lesniak <mail@mlesniak.com>
2014-01-17 19:25:34 +0100

    Correct way of including jquery.

--- src/main/java/com/mlesniak/template/BasePage.java
+++ src/main/java/com/mlesniak/template/BasePage.java
@@ -1,14 +1,14 @@
 package com.mlesniak.template;
 
 import com.mlesniak.template.navigation.NavigationBarPanel;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
 import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
+import org.apache.wicket.markup.head.PriorityHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.resource.JQueryResourceReference;
 
 public class BasePage extends WebPage {
     public BasePage() {
@@ -20,13 +20,6 @@ public class BasePage extends WebPage {
         super(parameters);
         addPageTitle();
         addNavigationBar();
-
-        add(new AjaxFallbackLink<Void>("fakeAjax") {
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-                // Ignored.
-            }
-        });
     }
 
     private void addNavigationBar() {
@@ -40,8 +33,6 @@ public class BasePage extends WebPage {
 
     @Override
     public void renderHead(IHeaderResponse response) {
-        super.renderHead(response);
-        response.render(JavaScriptHeaderItem.forReference(getApplication().getJavaScriptLibrarySettings()
-                .getJQueryReference()));
+        response.render(new PriorityHeaderItem(JavaScriptReferenceHeaderItem.forReference(JQueryResourceReference.get())));
     }
 }
